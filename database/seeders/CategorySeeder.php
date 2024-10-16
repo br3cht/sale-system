81f35675bbaed522e948d9be4ab87065566fd57a
br3cht <ant_175@hotmail.com>
2024-10-16 14:01:03 -0300

    refactor: added inital category in category seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -13,9 +13,51 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        Category::create([
-            'label' => 'televisor',
-            'slug' => 'televisor',
-        ]);
+        $categories = [
+            [
+                'label' => 'Eletrônicos',
+                'slug' => 'eletronicos',
+            ],
+            [
+                'label' => 'Moda',
+                'slug' => 'moda',
+            ],
+            [
+                'label' => 'Casa e Cozinha',
+                'slug' => 'casa-e-cozinha',
+            ],
+            [
+                'label' => 'Beleza e Cuidados Pessoais',
+                'slug' => 'beleza-e-cuidados-pessoais',
+            ],
+            [
+                'label' => 'Esportes e Lazer',
+                'slug' => 'esportes-e-lazer',
+            ],
+            [
+                'label' => 'Brinquedos e Jogos',
+                'slug' => 'brinquedos-e-jogos',
+            ],
+            [
+                'label' => 'Ferramentas e Construção',
+                'slug' => 'ferramentas-e-construcao',
+            ],
+            [
+                'label' => 'Alimentos e Bebidas',
+                'slug' => 'alimentos-e-bebidas',
+            ],
+            [
+                'label' => 'Livros',
+                'slug' => 'livros',
+            ],
+            [
+                'label' => 'Informática',
+                'slug' => 'informatica',
+            ],
+        ];
+
+        if(Category::count() == 0){
+            Category::upsert($categories,['id']);
+        }
     }
 }
