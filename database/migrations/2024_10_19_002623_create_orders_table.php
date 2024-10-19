d4423d00a8391babdb7ad10fc62dbeaf1583474b
br3cht <ant_175@hotmail.com>
2024-10-19 00:14:48 -0300

    refactor: added status

--- database/migrations/2024_10_19_002623_create_orders_table.php
+++ database/migrations/2024_10_19_002623_create_orders_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('status');
             $table->integer('valor_total');
             $table->foreignId('customer_id');
             $table->timestamps();
