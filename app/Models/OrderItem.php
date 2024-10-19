5a99181732374b29fbb85836d6140310ea9f5cee
br3cht <ant_175@hotmail.com>
2024-10-18 22:54:26 -0300

    refactor create new models

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function Order()
+    {
+        return $this->belongsTo(Order::class );
+    }
+}
