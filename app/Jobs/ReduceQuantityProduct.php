92084bcbdce42a2efadc8c76cd2725a2a43ae30d
br3cht <ant_175@hotmail.com>
2024-10-20 17:00:05 -0300

    refactor: added debug job

--- app/Jobs/ReduceQuantityProduct.php
+++ app/Jobs/ReduceQuantityProduct.php
@@ -7,6 +7,7 @@ use App\Services\ProductService;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
 
 class ReduceQuantityProduct implements ShouldQueue
 {
@@ -27,6 +28,7 @@ class ReduceQuantityProduct implements ShouldQueue
     public function handle(): void
     {
         foreach($this->itemCollection as $item){
+            Log::debug('Reduzindo a quantidade do produto: ' . $item->product->nome);
             $this->productService->reduceQuatity($item->product, $item->quantidade);
         }
     }
